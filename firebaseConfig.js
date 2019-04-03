668e9fe0b97c18b1d54890f98f1f76ab1cf1c937
Jérôme Bonnet <jbonnet@gtp-conseil.home>
2019-04-03 14:32:15 +0200

    ajout  fonction enregistrement bdd

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,6 @@
 const firebase = require("firebase");
 
-var config = {
+module.exports = {
     apiKey: "AIzaSyDn6IbNe7uA9J-Pib6iOI389SeVhkXv6rE",
     authDomain: "zigbee-88378.firebaseapp.com",
     databaseURL: "https://zigbee-88378.firebaseio.com",
@@ -8,5 +8,4 @@ var config = {
     storageBucket: "zigbee-88378.appspot.com",
     messagingSenderId: "1080511125053"
 };
-let app = firebase.initializeApp(config);
-const db = app;
+
--- routes/saveMove/saveMove.js
+++ routes/saveMove/saveMove.js
@@ -1,7 +1,14 @@
-const db = require('../../firebaseConfig');
+const firebase = require('firebase');
+const config = require('../../firebaseConfig');
+const db = firebase.initializeApp(config);
 
 const saveMove = (move, id) => {
-    console.log("bla");
+    //console.log("bla");
+    let myTable = db.database().ref('mesure');
+    myTable.push({ 'mouvement': move, 'sensorId': id  })
+    myTable.on('value', function(snapshot) {
+        console.log(snapshot.val())
+    });
 }
 
 module.exports = (req, res) => {
