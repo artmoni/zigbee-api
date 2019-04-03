28c2100f64de6287bde31ab9e29db0045834a338
Jérôme Bonnet <jbonnet@gtp-conseil.home>
2019-04-03 15:17:37 +0200

    route getData OK

--- routes/getData/getData.js
+++ routes/getData/getData.js
@@ -5,12 +5,16 @@ const db = require('../../firebaseConfig');
 
 const getData = () => {
     //console.log("bla");
+    let result="";
     let myTable = db.database().ref('mesure');
     myTable.on('value', function(snapshot) {
-        console.log(snapshot.val())
+        // console.log(snapshot.val())
+        result = snapshot.val()
     });
+    return result
 }
 
 module.exports = (req, res) => {
+    console.log(getData())
     res.send(getData())
  };
